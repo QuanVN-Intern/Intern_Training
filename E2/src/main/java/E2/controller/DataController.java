97dccf3ae5f420b57f23ee4605d823d5b80d3618
QuanVN-Intern <147361666+quanvn92@users.noreply.github.com>
2025-05-22 17:24:39 +0700

    new updates for E2

---
+++ E2/src/main/java/E2/controller/DataController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package E2.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+/**
+ *
+ * @author quant
+ */
+public class DataController {
+
+    private final RestTemplate restTemplate;
+
+    public DataController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/data")
+    public String getData() {
+        String url = "https://sse.dev/test?interval=1";
+        return restTemplate.getForObject(url, String.class);
+    }
+}
---
+++ E2/src/main/java/E2/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package E2.main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author quant
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
