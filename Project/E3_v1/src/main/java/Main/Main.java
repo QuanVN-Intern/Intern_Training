042809343a1cd50e079f03a7877343da47dc8609
QuanVN-Intern <147361666+quanvn92@users.noreply.github.com>
2025-06-05 17:27:09 +0700

    regular update for E3

--- Project/E3_v1/src/main/java/Main/Main.java
+++ Project/E3_v1/src/main/java/Main/Main.java
@@ -3,10 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Main;
-//import io.vertx.core.Vertx;
-//import io.vertx.ext.web.Router;
-//import io.vertx.ext.web.RoutingContext;
-//import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 /**
  *
  * @author quant
@@ -15,31 +15,31 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
-//        Vertx vertx = Vertx.vertx();
-//        Router router = Router.router(vertx);
-//
-//        // Register the REST API
-//        TestRest rest = new TestRest();
-//        router.route().handler(BodyHandler.create());
-//        router.get("/test/echo").handler(Main::handleEcho);
-//
-//        // Start the HTTP server
-//        vertx.createHttpServer()
-//            .requestHandler(router)
-//            .listen(8080, http -> {
-//                if (http.succeeded()) {
-//                    System.out.println("HTTP server started on port 8080");
-//                } else {
-//                    System.out.println("Failed to start HTTP server: " + http.cause());
-//                }
-//            });
-//    }
-//
-//    private static void handleEcho(RoutingContext context) {
-//        TestRest rest = new TestRest();
-//        String response = rest.echo();
-//        context.response()
-//            .putHeader("content-type", "text/html")
-//            .end(response);
+        Vertx vertx = Vertx.vertx();
+        Router router = Router.router(vertx);
+
+        // Register the REST API
+        TestRest rest = new TestRest();
+        router.route().handler(BodyHandler.create());
+        router.get("/test/echo").handler(Main::handleEcho);
+
+        // Start the HTTP server
+        vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(8080, http -> {
+                if (http.succeeded()) {
+                    System.out.println("HTTP server started on port 8080");
+                } else {
+                    System.out.println("Failed to start HTTP server: " + http.cause());
+                }
+            });
+    }
+
+    private static void handleEcho(RoutingContext context) {
+        TestRest rest = new TestRest();
+        String response = rest.echo();
+        context.response()
+            .putHeader("content-type", "text/html")
+            .end(response);
     }
 }
